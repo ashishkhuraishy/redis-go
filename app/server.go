40bbb04d4876334280f0b82ab6175a148dc8a4a6
ashishkhuraishy <ashishkhuraishy@gmail.com>
2023-05-14 15:32:53 +0530

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -20,9 +20,18 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		var data []byte
+
+		conn.Read(data)
+		resp := fmt.Sprintf("+PONG\r\n")
+		conn.Write([]byte(resp))
 	}
 }
